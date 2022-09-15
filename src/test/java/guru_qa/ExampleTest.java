beadcaaf665f52d0c0b73390b450b8055c31618d
RRGazizov <rrgazizov@gmail.com>
2022-09-15 16:14:23 +0300

    init commit from console

---
+++ src/test/java/guru_qa/ExampleTest.java
@@ -0,0 +1,12 @@
+package guru_qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+@Test
+void exampleTest(){
+    Assertions.assertTrue(2>1 );
+}
+}
